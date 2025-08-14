a94ca34e32120d8fd3cc8e6c19742788bd0fa7ee
Md Fazlul Karim Sium <sium.2305095@bau.edu.bd>
2025-08-14 23:17:05 +0600

    Create ADMIN_CONFIG.js

---
+++ ADMIN_CONFIG.js
@@ -0,0 +1,16 @@
+// GitHub credentials
+const GITHUB_USERNAME = "FK-Sium";           // replace with your GitHub username
+const GITHUB_TOKEN = "YOUR_PERSONAL_TOKEN"; // replace with your Personal Access Token
+
+// Admin credentials for your website
+const ADMIN_CONFIG = {
+    username: "fkadmin",                     // your chosen admin username
+    password: "StrongPassword123"           // your chosen admin password
+};
+
+// Export the config (for Node.js projects)
+module.exports = {
+    GITHUB_USERNAME,
+    GITHUB_TOKEN,
+    ADMIN_CONFIG
+};
