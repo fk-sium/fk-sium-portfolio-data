8f4cd6940ae6ab7cddeb98fd5099fceaf9fafbf0
Md Fazlul Karim Sium <sium.2305095@bau.edu.bd>
2025-08-14 23:22:50 +0600

    Update ADMIN_CONFIG.js

--- ADMIN_CONFIG.js
+++ ADMIN_CONFIG.js
@@ -1,11 +1,11 @@
 // GitHub credentials
-const GITHUB_USERNAME = "FK-Sium";           // replace with your GitHub username
-const GITHUB_TOKEN = "YOUR_PERSONAL_TOKEN"; // replace with your Personal Access Token
+const GITHUB_USERNAME = "fk-sium";           // replace with your GitHub username
+const GITHUB_TOKEN = "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"; // replace with your Personal Access Token
 
 // Admin credentials for your website
 const ADMIN_CONFIG = {
-    username: "fkadmin",                     // your chosen admin username
-    password: "StrongPassword123"           // your chosen admin password
+    username: "FKSium",                     // your chosen admin username
+    password: "Siumcancode@111"           // your chosen admin password
 };
 
 // Export the config (for Node.js projects)
